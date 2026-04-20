d647237e85e46cd882324b5a8d879061824236a3
Sumit-13082004 <sumitdas13082004@gmail.com>
2026-04-21 00:47:41 +0530

    instead of loose variables, grouping them

--- script.js
+++ script.js
@@ -1,52 +1,42 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
 let x = 10;
 let y = 10;
 let width = 30;
 let height = 30;
-ctx.fillRect(x, y, width, height);
 
-let direction = "RIGHT";
+const block = {
+  x: 10,
+  y: 10,
+  width: 30,
+  height: 30,
+  color: "green",
+  speed: 120, // 120px per second
+};
 
-function handleKey(e) {
-    if (e.key === "ArrowUp") {
-        ctx.clearRect(x, y, width, height);
-        y -= 20
-        ctx.fillRect(x, y, width, height);
-    } else if (e.key === "ArrowDown") {
-        ctx.clearRect(x, y, width, height);
-        y += 20
-        ctx.fillRect(x, y, width, height);
-    } else if (e.key === "ArrowLeft") {
-        ctx.clearRect(x, y, width, height);
-        x -= 20
-        ctx.fillRect(x, y, width, height);
-    } else if (e.key === "ArrowRight") {
-        ctx.clearRect(x, y, width, height);
-        x += 20
-        ctx.fillRect(x, y, width, height);
-    }
-}
+ctx.fillStyle = block.color;
+ctx.fillRect(block.x, block.y, block.width, block.height);
 
-let lastTime = performance.now();
-let frames = 0;
 
-function measure(time) {
-    frames++;
+// let direction = "RIGHT";
 
-    if (time - lastTime >= 1000) {
-        console.log("FPS:", frames);
-        frames = 0;
-        lastTime = time;
-    }
-
-    requestAnimationFrame(measure);
-}
-
-requestAnimationFrame(measure);
-
-document.addEventListener("keydown", handleKey);
-
-    
+// function handleKey(e) {
+//   if (e.key === "ArrowUp") {
+//     ctx.clearRect(x, y, width, height);
+//     y -= 20;
+//     ctx.fillRect(x, y, width, height);
+//   } else if (e.key === "ArrowDown") {
+//     ctx.clearRect(x, y, width, height);
+//     y += 20;
+//     ctx.fillRect(x, y, width, height);
+//   } else if (e.key === "ArrowLeft") {
+//     ctx.clearRect(x, y, width, height);
+//     x -= 20;
+//     ctx.fillRect(x, y, width, height);
+//   } else if (e.key === "ArrowRight") {
+//     ctx.clearRect(x, y, width, height);
+//     x += 20;
+//     ctx.fillRect(x, y, width, height);
+//   }
+// }
